ab876218a13be466ee22322cbe2f04987dcf87c1
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-10-21 14:16:52 +0530

    added cors for * yet

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import cors from 'cors';
 
 // dependencies
 import morgan from 'morgan';
@@ -8,12 +9,17 @@ import cookieParser from 'cookie-parser';
 // db connection
 import { connectDB } from './config/dbConnection.js';
 import { indexRoute } from './routes/indexRoutes.js';
+import { any } from 'zod';
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(morgan('dev'));
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(cookieParser());
+
 
 connectDB();
 
